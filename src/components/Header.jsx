cabb5d1e6da01af14ede1d158a2f290024c9f1b6
anzor <anzor.karmov@loyaltyplant.com>
2020-09-14 15:40:26 +0300

    learning react

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/b/be/Lineage_OS_Logo.png'/>
+    </header>
+}
+
+export default Header;
