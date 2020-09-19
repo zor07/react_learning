52d121054b9fe97487eda9f6b0ad0a54d73d4bc7
anzor <anzor.karmov@loyaltyplant.com>
2020-09-19 13:15:21 +0300

    my posts component

--- src/components/profile/MyPosts/MyPosts.jsx
+++ src/components/profile/MyPosts/MyPosts.jsx
@@ -7,9 +7,12 @@ const MyPosts = () => {
         <div className='posts'>
             My posts
             <div>
-                new post
+                <textarea></textarea>
+                <button>Add Post</button>
+                <button>Remove</button>
             </div>
             <div className={css.item}>
+                <img src='https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png'/>
                 post 1asd
             </div>
             <div>
