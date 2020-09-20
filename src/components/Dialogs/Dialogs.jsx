636d7c6c0811cd46ba3051a9f0b6491bcdb1b127
zor07 <anzor.karmov@gmail.com>
2020-09-20 14:29:14 +0300

    dialogs and css refactor

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import css from './Dialogs.module.css';
+
+const Dialogs = (props) => {
+
+    return (
+      <div>
+          Dialogs
+      </div>
+    );
+
+}
+
+export default Dialogs;
