f509c90a0d5ee231787d1cfc1f69dead2076fa51
zor07 <anzor.karmov@gmail.com>
2021-09-22 22:06:24 +0300

    lesson 12

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'/>
+        </div>
+        <div>
+            ava + description
+            <img src='http://toplogos.ru/images/logo-playboy.png'/>
+        </div>
+        <div>
+            My Posts
+            <div>
+                New Post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
