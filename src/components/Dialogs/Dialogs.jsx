a4471afb39192d4995f3182b509556350223286a
zor07 <anzor.karmov@gmail.com>
2021-09-25 11:58:53 +0300

    lesson 22. dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                <div className={s.dialog}>Anna</div>
-                <div className={s.dialog + ' ' + s.active}>Kristine</div>
-                <div className={s.dialog}>Ammy</div>
-                <div className={s.dialog}>Sasha</div>
+                <div className={s.dialog}>
+                    <NavLink activeClassName={s.active} to='/dialogs/1'>Anna</NavLink>
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink activeClassName={s.active} to='/dialogs/2'>Kristine</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink activeClassName={s.active} to='/dialogs/3'>Ammy</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink activeClassName={s.active} to='/dialogs/4'>Sasha</NavLink>
+                </div>
             </div>
-            <div className={s.separatorLine}></div>
+            <div className={s.separatorLine} />
             <div className={s.messages}>
                 <div className={s.message}>Hi</div>
                 <div className={s.message}>Hello</div>
