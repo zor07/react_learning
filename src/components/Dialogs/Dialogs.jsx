2df27ef6a77ac486700aba9c94cf15b70dfb120b
zor07 <anzor.karmov@gmail.com>
2021-09-25 11:45:11 +0300

    lesson 21. dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog}>Anna</div>
+                <div className={s.dialog + ' ' + s.active}>Kristine</div>
+                <div className={s.dialog}>Ammy</div>
+                <div className={s.dialog}>Sasha</div>
+            </div>
+            <div className={s.separatorLine}></div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Bye!</div>
+            </div>
         </div>
     );
 }
