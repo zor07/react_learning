8313618b7f2cc267c050c1fb8ed895f0e300f364
zor07 <anzor.karmov@gmail.com>
2021-09-26 22:01:02 +0300

    red design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import s from './Header.module.css'
 const Header = () => {
     return <header className={s.header}>
         <img src="https://static.tildacdn.com/tild3533-3066-4136-b166-656161323666/shtaf.png"/>
+        {/*<img src={'../img/output-onlinepngtools.png'}/>*/}
     </header>
 }
 
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,10 +4,13 @@ import s from './Post.module.css'
 const Post = (props) => {
     return (
         <div className={s.item}>
-            <img src='https://intim-mall.ru/images/thumbnails/1223/1000/detailed/250/154335.jpg'/>
-            {props.message}
-            <div>
-                <span>Like {props.likesCount}</span>
+            <div className={s.avatar}>
+                <img src='https://intim-mall.ru/images/thumbnails/1223/1000/detailed/250/154335.jpg'/>
+                {props.message}
+            </div>
+
+            <div className={s.likeBlock}>
+                <img src='https://www.digitalbizmagazine.com/wp-content/uploads/2019/04/corazon.png'/> <span>{props.likesCount}</span>
             </div>
         </div>
     );
