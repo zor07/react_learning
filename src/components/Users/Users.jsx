8f9b1c33f1fc7c03bcd5a68c6942a4268d18b5a1
zor07 <anzor.karmov@gmail.com>
2021-10-21 12:34:37 +0300

    lesson 56

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,80 +1,31 @@
 import React from "react";
-import css from "./Users.module.css";
-import * as axios from "axios";
 import User from "./User/User";
+import css from "./Users.module.css";
 
+const Users = (props) => {
 
-class Users extends React.Component {
+    let users = props.users
+        .map(user => <User user={user}
+                           follow={(userId) => props.follow(userId) }
+                           unfollow={(userId) => props.unfollow(userId)}/>)
 
-    _setCurrentPage = (page) => {
-        this.props.setCurrentPage(page);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-            })
-    }
+    let pageInput = <input type="number"
+                           className={css.pageInput}
+                           value={props.currentPage}
+                           onChange={props.onPageInputChange}/>;
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount)
-            })
-    }
-
-    goToFirsPage = () => {
-        this._setCurrentPage(1);
-    }
-
-    goToLastPage = () => {
-        this._setCurrentPage(Math.ceil(this.props.totalUsersCount / this.props.pageSize));
-    }
-
-    goToPrevPage = () => {
-        if (this.props.currentPage !== 1) {
-            this._setCurrentPage(this.props.currentPage - 1);
-        }
-    }
-
-    goToNextPage = () => {
-        if (this.props.currentPage !== Math.ceil(this.props.totalUsersCount / this.props.pageSize)) {
-            this._setCurrentPage(this.props.currentPage + 1);
-            console.log(this.props.currentPage)
-        }
-    }
-
-    onPageInputChange = (e) => {
-        let newPage = parseInt(e.target.value);
-        if (newPage >= 1 && newPage <= Math.ceil(this.props.totalUsersCount / this.props.pageSize)) {
-            this._setCurrentPage(newPage);
-        }
-    }
-
-    render() {
-        let users = this.props.users
-            .map(user => <User user={user}
-                               follow={(userId) => this.props.follow(userId) }
-                               unfollow={(userId) => this.props.unfollow(userId)}/>)
-
-        let pageInput = <input type="number"
-                               className={css.pageInput}
-                               value={this.props.currentPage}
-                               onChange={this.onPageInputChange}/>;
-
-        return (<div className={css.users}>
-                <h3>Users</h3>
-                <div className={css.pagination}>
-                    <span><button onClick={this.goToFirsPage}>{'<<'}</button>  </span>
-                    <span><button onClick={this.goToPrevPage}>{'<'}</button>  </span>
-                    <span><button onClick={this.goToNextPage}>{'>'}</button>  </span>
-                    <span><button onClick={this.goToLastPage}>{'>>'}</button>  </span>
-                    <span>Page {pageInput} of {Math.ceil(this.props.totalUsersCount / this.props.pageSize)}</span>
-                </div>
-                {users}
+    return (<div className={css.users}>
+            <h3>Users</h3>
+            <div className={css.pagination}>
+                <span><button onClick={props.goToFirsPage}>{'<<'}</button>  </span>
+                <span><button onClick={props.goToPrevPage}>{'<'}</button>  </span>
+                <span><button onClick={props.goToNextPage}>{'>'}</button>  </span>
+                <span><button onClick={props.goToLastPage}>{'>>'}</button>  </span>
+                <span>Page {pageInput} of {props.pagesTotal}</span>
             </div>
+            {users}
+        </div>
+    )
 
-        )
-    }
 }
-
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,71 @@
 import React from "react";
 import {connect} from "react-redux";
 import {followAc, setCurrentPageAc, setTotalUsersCountAc, setUsersAc, unfollowAc} from "../../redux/users-reducer";
+import * as axios from "axios";
 import Users from "./Users";
 
+class UsersAPIComponent extends React.Component {
+
+    _setCurrentPage = (page) => {
+        this.props.setCurrentPage(page);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            })
+    }
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    goToFirsPage = () => {
+        this._setCurrentPage(1);
+    }
+
+    goToLastPage = () => {
+        this._setCurrentPage(Math.ceil(this.props.totalUsersCount / this.props.pageSize));
+    }
+
+    goToPrevPage = () => {
+        if (this.props.currentPage !== 1) {
+            this._setCurrentPage(this.props.currentPage - 1);
+        }
+    }
+
+    goToNextPage = () => {
+        if (this.props.currentPage !== Math.ceil(this.props.totalUsersCount / this.props.pageSize)) {
+            this._setCurrentPage(this.props.currentPage + 1);
+            console.log(this.props.currentPage)
+        }
+    }
+
+    onPageInputChange = (e) => {
+        let newPage = parseInt(e.target.value);
+        if (newPage >= 1 && newPage <= Math.ceil(this.props.totalUsersCount / this.props.pageSize)) {
+            this._setCurrentPage(newPage);
+        }
+    }
+
+    render() {
+        debugger
+        return (
+            <Users users={this.props.users}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}
+                   currentPage={this.props.currentPage}
+                   pagesTotal={ Math.ceil(this.props.totalUsersCount / this.props.pageSize)}
+                   onPageInputChange={this.onPageInputChange}
+                   goToFirsPage={this.goToFirsPage}
+                   goToPrevPage={this.goToPrevPage}
+                   goToNextPage={this.goToNextPage}
+                   goToLastPage={this.goToLastPage}/>
+        )
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -33,4 +96,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent)
