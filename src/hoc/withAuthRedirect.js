f9c6645f83431c72c39a51b040c777a78a3ec20e
zor07 <anzor.karmov@gmail.com>
2021-12-10 13:55:59 +0300

    lesson 78 fix withAuthRedirect

--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -1,49 +1,25 @@
-import React from "react";
-import {Route, Routes, useNavigate} from "react-router-dom";
+import React, {Component, useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 import {connect} from "react-redux";
-import Login from "../components/Login/Login";
-
 
 export const withAuthRedirect = (Component) => {
-
-    let mapStateToPropsForRedirect = (state) => ({
-        isAuth: state.auth.isAuth
-    })
-
-    // class RedirectComponent extends React.Component {
-    //
-    //     constructor(props, context) {
-    //         super(props, context);
-    //         this.navigate = useNavigate();
-    //     }
-    //
-    //
-    //     render(){
-    //         if (!this.props.isAuth) {
-    //             this.navigate('/login');
-    //             // return (
-    //             //     <Routes>
-    //             //         <Route path="about" element={<Login/>} />
-    //             //     </Routes>
-    //             //
-    //             // )
-    //         }
-    //
-    //         return <Component {...this.props}/>
-    //     }
-    // }
-
-    const RedirectComponent = (props) => {
-
+    const Wrapper = (props) => {
         const navigate = useNavigate();
 
-        if (!props.isAuth) {
-            navigate('/login')
-        }
+        useEffect(() => {
+            if (!props.isAuth) {
+                navigate("/login")
+            }
+        })
 
         return <Component {...props} />
     }
 
-    return connect(mapStateToPropsForRedirect)(RedirectComponent);
+    let mstp = (state) => ({
+        isAuth: state.auth.isAuth
+    })
 
+    return  connect(mstp)(Wrapper);
 }
+
+
